55acd47695fa9dad0f00221ce5bf1aea3cb0e8d4
George Castillo <gmcastil@gmail.com>
2017-11-26 18:19:03 -0700

    Moved the huge docstring out of the Python source code and into the README.

--- src/oddball.py
+++ src/oddball.py
@@ -1,73 +1,7 @@
 """
-A very oddball assembler for the MOS 6502 instruction set
-
-Unlike most assemblers which generate machine code for a given architecture,
-this one generates memory initialization files (.mif) used for initial
-configuration of Xilinx block memories.  The primary use case is to create
-inputs to RTL simulations from 6502 assembly language source code and optionally
-provided memory maps containing additional data to place in the final memory
-initialization file.
-
-It accepts .asm text of the following format:
-
-         .directive     operands
-label:   instr          operands       ; comments
-
-The only supported directive is .org, which sets the current origin to a new
-value.  This is used to set the program counter during assembly.  For example,
-.org $8000 would instruct the assembler to start placing code at address $8000.
-At least one instance of this directive should be used to indicate where to
-begin placing instructions.
-
-Since this use case is rather strange, there are a couple things to keep in
-mind:
-
-* The 'assembled' output will always be 64KB in size, since that is the size of
-  the 6502 address space.
-* All non-program bytes will be populated with NOP opcodes
-* Since the purpose of the output is to simulate the 6502 address space,
-  additional bytes will appear in the output, notably the address of the reset
-  vector and interrupt handling routines in addresses $fffa through $ffff
-
-Map Files:
-
-If additional data are desired to be placed in memory, a .map file of the
-following format can be provided as well:
-
-$addr: $data
-
-Where $addr is a string representing a 16-bit hex value and $data a string
-representing an 8-bit hex value.  Here is an example:
-
-$9000: $33
-$9001: $ff
-
-This would place the value $33 at address $9000 and $ff at address $9001.  Since
-data placement occurs after assembly, it is possible to overwrite machine code
-with data in memory.  No checking for this is done though.
-
-Usage:
-
-  asm6502 [options] [file]
-
-Options:
-
-  -c, --coe-only    Generates the interim coefficients file instead of a
-                    .mif file.  Possibly useful for manually hacking of source
-                    code.
-  -m, --with-map    Use this file to place additional data into memory
-  -o, --output      Output filename to use (optional).  Default is to use the
-                    input filename with .mif
-
-Development:
-
-  - Iterate over stripped out source code containing only code or directives
-  - Break source file up into blocks delineated by .org directives
-  - Return a dictionary containing the origin addresses (keys) and code
-    blocks (values)
+A very strange assembler for the MOS 6502 instruction set
 
 """
-import pdb
 import sys
 from collections import namedtuple
 
